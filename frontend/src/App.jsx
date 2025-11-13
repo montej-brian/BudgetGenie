3ab11949438237f90b637cc876d1b8007f84120f
montej <chachixzeedon@gmail.com>
2025-11-13 23:36:34 +0300

    fix: Remove unused Header component to fix build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Home from "./pages/Home";
-import Header from "./components/Header";
 import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Header />
       <Home />
       <Analytics />
     </div>
