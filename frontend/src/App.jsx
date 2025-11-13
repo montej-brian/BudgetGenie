f22ba00fab6bfe1e5920b634b05bcd0e8a804948
montej <chachixzeedon@gmail.com>
2025-11-13 23:18:31 +0300

    analytics update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       <Header />
       <Home />
+      <Analytics />
     </div>
   );
 };
